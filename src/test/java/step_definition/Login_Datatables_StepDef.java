a7637bd9007f86f0ef5d7741548b6ebc13e56902
Mohamed Nheri <nherimohamed@gmail.com>
2024-03-16 11:41:06 -0400

    Datatables

--- src/test/java/step_definition/Login_Datatables_StepDef.java
+++ src/test/java/step_definition/Login_Datatables_StepDef.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 import java.util.List;
+import java.util.Map;
 
 public class Login_Datatables_StepDef {
     @Given("I open my application")
@@ -21,11 +22,56 @@ public class Login_Datatables_StepDef {
     @Given("I login with following credentials")
     public void i_login_with_following_credentials(DataTable dataTable) {
         List<List<String>> loginCreds = dataTable.asLists(String.class);
-        System.out.println("Username is : " + loginCreds.get(0).get(0));
-        System.out.println("Password is : " + loginCreds.get(0).get(1));
-        System.out.println("Username is : " + loginCreds.get(1).get(0));
-        System.out.println("Password is : " + loginCreds.get(1).get(1));
+//        System.out.println("Username is : " + loginCreds.get(0).get(0));
+//        System.out.println("Password is : " + loginCreds.get(0).get(1));
+//        System.out.println("Username is : " + loginCreds.get(1).get(0));
+//        System.out.println("Password is : " + loginCreds.get(1).get(1));
+
+        //Using for each to loop throught each of the item inside the list
+//        for(List<String> val : loginCreds){
+//            for(String creds : val){
+//                System.out.println("Creds are : " + creds);
+//            }
+//        }
+
+        for(int i  = 0 ; i < loginCreds.size() ; i++){
+            for(int j = 0 ; j < loginCreds.get(i).size() ; j++){
+                if( j ==  0 ){
+                    System.out.println("Username is : " + loginCreds.get(i).get(j));
+                }else{
+                    System.out.println("Password is : " + loginCreds.get(i).get(j));
+                }
+            }
+        }
+
 
     }
+
+
+    @Given("I login with these following credentials")
+    public void i_login_with_these_following_credentials(DataTable dataTable) {
+        List<Map<String,String>> loginCreds = dataTable.asMaps();
+        System.out.println(loginCreds);
+//        System.out.println(loginCreds.get(0).get("username"));
+//        System.out.println(loginCreds.get(0).get("password"));
+//        System.out.println(loginCreds.get(1).get("username"));
+//        System.out.println(loginCreds.get(1).get("password"));
+//        System.out.println(loginCreds.get(2).get("username"));
+//        System.out.println(loginCreds.get(2).get("password"));
+
+        for(Map<String,String> user : loginCreds){
+            String userName = user.get("username");
+            String password = user.get("password");
+            System.out.println("Username is : " + userName +" ---- Password is : " + password);
+        }
+
+
+
+    }
+
+
+
+
+
 
 }
--- src/test/resources/features/Login_Datatables.feature
+++ src/test/resources/features/Login_Datatables.feature
@@ -9,3 +9,20 @@ Feature: Login to application
     And I login with following credentials
       | admin    | pass123 |
       | admin123 | pas345  |
+      | admin456 | pass678 |
+
+  Scenario: Login to application with credentials
+    Given I open my application
+    And I login with these following credentials
+    | username | password |
+    | admin    | pass123 |
+    | admin123 | pas345  |
+    | admin456 | pass678 |
+
+
+#    [   { username : admin  , password: pass 123},
+#
+#        { username : admin123  , password: pas345},
+#
+#        { username : admin456  , password: pass678}
+#  ]
